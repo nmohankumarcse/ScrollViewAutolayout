7cdc20ea2c3cee0f75c7229fd87fe26844f4e701
Mohankumar <nmohankumar.cse@gmail.com>
2018-03-20 17:48:41 +0530

    Initial Commit

---
+++ ScrollViewSample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ScrollViewSample
+//
+//  Created by Mohankumar on 20/03/18.
+//  Copyright © 2018 Mohankumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ScrollViewSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollViewSample
+//
+//  Created by Mohankumar on 20/03/18.
+//  Copyright © 2018 Mohankumar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
